7b1f932e7ea9bfab95c2b58851b2f9dcd25baf6b
Ahmet KOYUN <ahmetkoyun90@gmail.com>
2019-05-24 14:21:20 +0300

    v2.9.2

---
+++ build/precache-manifest.278fabbe1b6bab2b5f7cbdd463d45647.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4f9b52937c8ea29953dcc0077fc386c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30217a7f171c9f897261",
+    "url": "/static/css/main.2978123d.chunk.css"
+  },
+  {
+    "revision": "bbde7e5d4598ede6b856",
+    "url": "/static/js/2.ddab3055.chunk.js"
+  },
+  {
+    "revision": "30217a7f171c9f897261",
+    "url": "/static/js/main.4ab40101.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "131ad39d894061b383a364fca192fa24",
+    "url": "/static/media/Icon.131ad39d.svg"
+  },
+  {
+    "revision": "b7986f762f0969752aaf9172dd9698e7",
+    "url": "/static/media/Logo.b7986f76.svg"
+  }
+]);
